6c95605acc3268ba0d1b2e8902b8341f0ae1294e
unknown <mindc1jun17@iiht.tech>
2022-08-10 14:40:26 +0530

    First Commit

---
+++ src/main/java/com/helloworldexample/springboot/app/SpringbootHelloworldExampleApplication.java
@@ -0,0 +1,14 @@
+package com.helloworldexample.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloworldExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloworldExampleApplication.class, args);
+	}
+
+}
